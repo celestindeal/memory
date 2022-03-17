04f523a42fb212e4561283b723bcac36755024aa
celestindeal <66480838+celestindeal@users.noreply.github.com>
2022-03-17 11:15:48 +0100

    mise en place

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,173 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_distribuer = new System.Windows.Forms.Button();
+            this.ilSabotDeCartes = new System.Windows.Forms.ImageList(this.components);
+            this.Test = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox4, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(27, 20);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 112F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(710, 189);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(534, 3);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(173, 183);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pb_04);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(357, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(171, 183);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pb_03);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(180, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(171, 183);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pb_02);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(171, 183);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pb_01);
+            // 
+            // button_distribuer
+            // 
+            this.button_distribuer.Location = new System.Drawing.Point(85, 322);
+            this.button_distribuer.Name = "button_distribuer";
+            this.button_distribuer.Size = new System.Drawing.Size(150, 39);
+            this.button_distribuer.TabIndex = 2;
+            this.button_distribuer.Text = "Distribuer";
+            this.button_distribuer.UseVisualStyleBackColor = true;
+            this.button_distribuer.Click += new System.EventHandler(this.btn_Distribuer);
+            // 
+            // ilSabotDeCartes
+            // 
+            this.ilSabotDeCartes.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ilSabotDeCartes.ImageStream")));
+            this.ilSabotDeCartes.TransparentColor = System.Drawing.Color.Transparent;
+            this.ilSabotDeCartes.Images.SetKeyName(0, "DosCarte.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(1, "Abeille.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(2, "Autruches.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(3, "Canard.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(4, "Chameau.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(5, "Chien.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(6, "ChienGamelle.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(7, "ChienMechant.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(8, "ChienSavant.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(9, "Chimpanze.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(10, "Coccinelle.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(11, "Crabe.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(12, "Crocodile.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(13, "Dauphin.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(14, "Ecureuil.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(15, "Hanneton.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(16, "Hibou.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(17, "Hippocampe.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(18, "Homard.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(19, "Kangourou.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(20, "Lapin.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(21, "LapinChapeau.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(22, "LapinRieur.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(23, "Lion.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(24, "MacareuxMoine.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(25, "Mouette.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(26, "Ocelot.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(27, "OiseauCage.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(28, "OiseauChante.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(29, "OiseauPic.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(30, "Orycterope.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(31, "Papillon.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(32, "PoissonBocal.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(33, "PoissonBulle.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(34, "PoissonPapillon.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(35, "PorcEpic.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(36, "PorcEpic2.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(37, "Renne.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(38, "Renne2.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(39, "Serpent.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(40, "Taureau.jpg");
+            this.ilSabotDeCartes.Images.SetKeyName(41, "Vache.jpg");
+            // 
+            // Test
+            // 
+            this.Test.Location = new System.Drawing.Point(241, 322);
+            this.Test.Name = "Test";
+            this.Test.Size = new System.Drawing.Size(150, 39);
+            this.Test.TabIndex = 3;
+            this.Test.Text = "Text";
+            this.Test.UseVisualStyleBackColor = true;
+            this.Test.Click += new System.EventHandler(this.btn_Test);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Test);
+            this.Controls.Add(this.button_distribuer);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_distribuer;
+        private System.Windows.Forms.ImageList ilSabotDeCartes;
+        private System.Windows.Forms.Button Test;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,13 +8,113 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//importer le 'pakage' pour avoir des nombres aléatoires
+using System.Windows.Forms;
+using dllLoto;
+
 namespace meory
 {
     public partial class Form1 : Form
     {
+        int nbCartesDansSabot; // Nombre de cartes dans le sabot (en fait nombre
+                               // d'images dans le réservoir)
+        int nbCartesSurTapis; // Nombre de cartes sur le tapis
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pb_01(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pb_02(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb_03(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb_04(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb_05(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb_06(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb_07(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb_08(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Distribution_Aleatoire()
+        {
+            LotoMachine hasard = new LotoMachine(10);
+            int[] tImagesCartes = hasard.TirageAleatoire(nbCartesSurTapis, false);
+            // La série d'entiers retournée par la LotoMachine correspondra
+            // aux indices des cartes dans le "sabot"
+            // Affectation des images aux picturebox
+            PictureBox carte;
+            int i_image;
+            for (int i_carte = 0; i_carte < nbCartesSurTapis; i_carte++)
+            {
+                carte = (PictureBox)tableLayoutPanel1.Controls[i_carte];
+                i_image = tImagesCartes[i_carte + 1]; // i_carte + 1 à cause
+                                                      // des pbs d'indices
+                carte.Image = ilSabotDeCartes.Images[i_image];
+            }
+        }
+
+        private void btn_Distribuer(object sender, EventArgs e)
+        {
+            // On récupère le nombre d'images dans le réservoir :
+            nbCartesDansSabot = ilSabotDeCartes.Images.Count - 1;
+    
+            // s’il y a 40 images au total *
+            // On récupère également le nombre de cartes à distribuées sur la tapis
+            // autrement dit le nombre de contrôles présents sur le conteneur
+            nbCartesSurTapis = tableLayoutPanel1.Controls.Count;
+            // On effectue la distribution (aléatoire) proprement dite
+            Distribution_Aleatoire();
+        }
+
+        private void btn_Test(object sender, EventArgs e)
+        {
+            // On utilise la LotoMachine pour générer une série aléatoire
+            // On fixe à 49 le nombre maxi que retourne la machine
+            LotoMachine hasard = new LotoMachine(10);
+            // On veut une série de 6 numéros distincts parmi 49 (comme quand on joue au loto)
+            int[] tirageLoto = hasard.TirageAleatoire(6, false);
+            // false veut dire pas de doublon : une fois qu'une boule est sortie,
+            // elle ne peut pas sortir à nouveau ;-)
+            // La série d'entiers retournée par la LotoMachine correspond au loto
+            // affiché sur votre écran TV ce soir...
+            string grilleLoto = "* ";
+            for (int i = 1; i <= 6; i++)
+            {
+                grilleLoto = grilleLoto + tirageLoto[i] + " * ";
+            }
+            MessageBox.Show(grilleLoto, "Tirage du LOTO ce jour !");
+
         }
     }
 }
